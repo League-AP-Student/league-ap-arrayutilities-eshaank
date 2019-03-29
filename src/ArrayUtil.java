5edba07ceaed7ed51c1632438f0d2a909819df83
League Student <league@iMac-8.attlocal.net>
2019-03-28 18:16:16 -0700

    asdf
    asdf

--- src/ArrayUtil.java
+++ src/ArrayUtil.java
@@ -1,19 +1,27 @@
 
 public class ArrayUtil {
-	/** Shifts each array element to the next higher index, discarding the 
-	 *  original last element, and inserts the new number at the front. 
-	 *  @param arr the array to manipulate
-	 * 		Precondition: arr.length > 0
-	 *  @param num the new number to insert at the front of arr
-	 * 		Postcondition: The original elements of arr have been shifted to 
-	 * 				 	   the next higher index, and arr[0] == num. 
-	 * 				  	   The original element at the highest index has been 
-	 * 	  				   discarded.
-	*/
-	public static void shiftArray(int[] arr, int num) 
-	{ /* to be implemented in part (a) */ 
-			
+	/**
+	 * Shifts each array element to the next higher index, discarding the original
+	 * last element, and inserts the new number at the front.
+	 * 
+	 * @param arr
+	 *            the array to manipulate Precondition: arr.length > 0
+	 * @param num
+	 *            the new number to insert at the front of arr Postcondition: The
+	 *            original elements of arr have been shifted to the next higher
+	 *            index, and arr[0] == num. The original element at the highest
+	 *            index has been discarded.
+	 */
+	public static int[] shiftArray(int[] arr, int num) { /* to be implemented in part (a) */
+		int[] testArr = new int[arr.length];
+		testArr[0] = num;
+		for (int i = 1; i < arr.length; i++) {
+
+			testArr[i] = arr[i - 1];
+
+		}
+		return testArr;
 	}
-	
+
 	// There may be methods that are not shown.
 }
--- src/NumberMatrix.java
+++ src/NumberMatrix.java
@@ -1,42 +1,50 @@
 
 public class NumberMatrix {
 	private int[][] matrix;
-	
-	/** Constructs a number matrix. */ 
-	public NumberMatrix(int[][] m) 
-	{ matrix = m; }
-	
-	/**Shifts each matrix element to the next position in row-major order 
-	 * and inserts the new number at the front. The last element in the last 
-	 * row is discarded.
-	 * @param num the new number to insert at the front of matrix
-	 * Postcondition:The original elements of matrix have been shifted to 
-	 * 				 the next higher position in row-major order, and
-	 * 				 matrix[0][0] == num.
-	 * 				 The original last element in the last row is discarded.
-	 */	
-	public void shiftMatrix(int num)
-	{ /* to be implemented in part (b) */ 
-		
-		
-		
+
+	/** Constructs a number matrix. */
+	public NumberMatrix(int[][] m) {
+		matrix = m;
 	}
-	
-	/**Rotates each matrix element to the next higher position in row-major 
-	 * order.
-	 * Postcondition: The original elements of matrix have been shifted
-	 * 				  to the next higher position in row-major order, and 
-	 * 				  matrix[0][0] == the original last element.
+
+	/**
+	 * Shifts each matrix element to the next position in row-major order and
+	 * inserts the new number at the front. The last element in the last row is
+	 * discarded.
+	 * 
+	 * @param num
+	 *            the new number to insert at the front of matrix Postcondition:The
+	 *            original elements of matrix have been shifted to the next higher
+	 *            position in row-major order, and matrix[0][0] == num. The original
+	 *            last element in the last row is discarded.
 	 */
-	public void rotateMatrix()
-	{ /* to be implemented in part (c) */ 
-		
+	public int[] shiftMatrix(int num) { /* to be implemented in part (b) */
+
+		int[] array = new int[matrix.length * matrix[0].length];
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				array[i * matrix[0].length + j] = matrix[i][j];	
+			}
+			
+		}
+
+		return ArrayUtil.shiftArray(array, num);
 	}
-	
+
+	/**
+	 * Rotates each matrix element to the next higher position in row-major order.
+	 * Postcondition: The original elements of matrix have been shifted to the next
+	 * higher position in row-major order, and matrix[0][0] == the original last
+	 * element.
+	 */
+	public void rotateMatrix() { /* to be implemented in part (c) */
+
+	}
+
 	// There may be instance variables, constructors, and methods that are not
 	// shown.
-	
-	public int[][] getMatrix(){
+
+	public int[][] getMatrix() {
 		return matrix;
 	}
 }
